c9c47609413b8479b6521d58655c703fa3ef002d
sushantpj <sushantpj2620@gmail.com>
2023-11-18 00:03:22 +0530

    initial commit2

--- Middleware/deletecookie.js
+++ Middleware/deletecookie.js
@@ -1,8 +1,10 @@
 
 
-export const deletecookie = (Token,res,msg) =>{
+export const deletecookie = (Token, res, msg) => {
     res.status(200).cookie(Token, "", {
         expires: new Date(Date.now()),
+        sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Develpoment" ? false : true,
     }).json({
         success: true,
         message: msg
